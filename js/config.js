a102a57c4e56190301fde7001a1cf7acb103df83
DwayneKD <dwayneriley95@gmail.com>
2025-05-01 21:47:11 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_5XUjU2ePa', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6raju8codclvq3jcdulmomo8o4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_zHsDw2Xgv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7utckboq915t8d5m4766ug0g03', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
